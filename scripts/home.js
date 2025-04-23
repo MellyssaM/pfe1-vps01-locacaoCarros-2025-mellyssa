69fb3ca8522a5b1b03fa57a31bbea237bc254f9f
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-04-23 08:22:34 -0300

    updated

--- scripts/home.js
+++ scripts/home.js
@@ -21,8 +21,9 @@ function exibirCards() {
         const card = document.createElement('div');
         card.classList.add('card');
         card.innerHTML = `
-            <img src="${produto.imagem}" alt="${produto.nome}">
+            <img src="${produto.imagem}">
             <h2>${produto.modelo}</h2>
+            <p>${produto.marca}</p>
             <p>R$ ${produto.valor_diaria.toFixed(2)}</p>
             <button onclick="mostrarDetalhes(${indice})">Detalhes</button>
         `;
@@ -40,32 +41,10 @@ function mostrarDetalhes(indice) {
     const botao = document.querySelector('#detalhes .rodape button');
 
     detalhes.classList.remove('oculto');
-    titulo.innerHTML = produtos[indice].nome;
+    titulo.innerHTML = produtos[indice].marca;
     imagem.src = produtos[indice].imagem;
     modelo.innerHTML = produtos[indice].modelo;
     ano.innerHTML = produtos[indice].ano;
     valor_diaria.innerHTML = `R$ ${produtos[indice].valor_diaria.toFixed(2)}`;
     botao.setAttribute('onclick', `adicionarCarrinho(${indice})`);
-}
-
-function adicionarCarrinho(indice) {
-    const produto = produtos[indice];
-    //Verifica se o produto já está no carrinho
-    const produtoExistente = carrinho.find(item => item.id === produto.id);
-    if (produtoExistente) {
-        produtoExistente.quantidade += 1; // Incrementa a quantidade
-        produtoExistente.total = produtoExistente.quantidade * (produto.marca + frete); // Atualiza o total
-        // Atualiza o total do produto existente no carrinho
-        const index = carrinho.indexOf(produtoExistente);
-        if (index > -1) {
-            carrinho[index] = produtoExistente; // Atualiza o produto existente no carrinho
-        }
-    } else {
-        produto.quantidade = 1; // Define a quantidade inicial como 1
-        produto.total = produto.marca + frete; // Define o total inicial
-        carrinho.push(produto);
-    }
-    localStorage.setItem('carrinho', JSON.stringify(carrinho));
-    window.location.href = 'carrinho.html';
-    const detalhes = document.getElementById('detalhes');
 }
